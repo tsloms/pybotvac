99619c918e01920579d319df3c2454dfdbfda1e5
cobirnm <45829329+cobirnm@users.noreply.github.com>
2025-03-10 15:58:46 +0000

    Update vorwerk.py

--- pybotvac/vorwerk.py
+++ pybotvac/vorwerk.py
@@ -11,4 +11,4 @@ class Vorwerk(Vendor):
     scope = ["openid", "email", "profile", "read:current_user", "offline_access"]
     audience = "https://mykobold.eu.auth0.com/userinfo"
     source = "vorwerk_auth0"
-    cert_path = None
+    cert_path = "None"
